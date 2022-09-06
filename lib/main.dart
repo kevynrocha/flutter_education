02c37004e0e871f5facceb700f1dde60cd3d48a8
Kevyn Rocha <kevyn_oliver@hotmail.com>
2022-09-06 01:22:52 -0300

    refactor: force portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/services.dart';
 import 'app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
